c611ce34767a1aa3583cdf6e5cde75fc2fd2d717
Piyush <pbajpai207@gmail.com>
2025-06-07 22:43:58 +0530

    handling 404 error on vercel

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,25 +1,24 @@
 import './css/App.css'
 import Home from "./pages/Home"
 import Fav from './pages/Favs';
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import NavBar from './components/NavBar';
 import { MovieProvider } from './context/MovieContext';
 
 function App() {
-  const MovieNumber=1;
-
+  const MovieNumber = 1;
+  
   return (
-   <MovieProvider>
-    <NavBar />
-    <main className='main-content'>
-     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/favorites" element={<Fav/>}/>
-     </Routes>
-   </main>
-   </MovieProvider>
+    <MovieProvider>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Fav />} />
+        </Routes>
+      </main>
+    </MovieProvider>
   );
 }
-
 
 export default App
